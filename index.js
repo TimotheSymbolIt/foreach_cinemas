4c401f20c5b5264619455ce53424e5ca91203329
TimotheSymbolIt <timothe.gauquelin@symbol-it.fr>
2024-02-28 17:02:38 +0100

    Ajout de la fonction 'mettre_favori'

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function fermer_publicite() {
 	var block_publicite = document.getElementById("publicite");
 	block_publicite.style.display = "none";
 }
+
+function mettre_favori() {
+	var icone_coeur = document.querySelector(".film_titre_icone .material-icons");
+
+	if (icone_coeur.textContent === "favorite") {
+		icone_coeur.textContent = "favorite_outline";
+		icone_coeur.style.color = "black";
+	} else {
+		icone_coeur.textContent = "favorite";
+		icone_coeur.style.color = "red";
+	}
+}
